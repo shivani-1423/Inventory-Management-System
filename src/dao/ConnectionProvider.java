79b57282801b3ae72021d7f6b8195d9b98059e6b
Shivani <shivanirsakhare@gmail.com>
2025-08-13 23:50:00 +0530

    Add files via upload

---
+++ src/dao/ConnectionProvider.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author UMMEHAANI BAGWAN
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory?useSSL=false","root","11111");
+            return con;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
---
+++ src/dao/InventoryUtls.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author UMMEHAANI BAGWAN
+ */
+public class InventoryUtls {
+    public static String billPath="C:\\Users\\UMMEHAANI BAGWAN\\OneDrive\\Documents\\advancejavapro";
+    
+}
